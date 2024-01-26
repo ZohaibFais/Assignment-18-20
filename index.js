17cb0e85f08fc881d0a894c417894fe19f08b901
ZohaibFais <murtuza.tcc@gmail.com>
2024-01-26 19:22:50 +0500

    Q5. complete

---
+++ index.js
@@ -0,0 +1,51 @@
+//Q1.
+// for (var i = 0; i < 6; i++) {
+//   document.write("Hello World " + "<br>");
+// }
+
+//Q2.
+
+// for (var i =1; i<11; i++){
+//     document.write(i +"<br>")
+// }
+
+//Q3.
+
+// var number = parseInt(prompt("Enter a number to print its multiplication table: "))
+// var length = parseInt(prompt("Enter length of multiplication table: "))
+
+// document.write("Multiplication table of: " + " "+ number)
+// document.write("<br>")
+// document.write("Length: " + " "+ length)
+// document.write("<br>")
+// document.write("<br>")
+
+// for (var i =1; i<length+1; i++){
+//     var result = number * i
+//     document.write(number + " " + "*" + i + " " + "=" + " "+ result)
+//     document.write("<br>")
+
+// }
+
+//Q4.
+
+// var ary = ["Nokia", "Samsung" ,"Apple", "Sony", "Huawei"]
+
+// for (var i =0; i<ary.length; i++ ){
+//     document.write(ary[i] + "<br>")
+// }
+
+
+//Q5.
+
+// var fruits = ["apple", "banana", "mango", "orange", "strawberry"]
+
+// for (i=0; i<fruits.length; i++){
+//     document.write(fruits[i] + "<br>")
+// }
+// document.write("<br>")
+// document.write("<br>")
+
+// for (i=0; i<fruits.length; i++){
+//     document.write("Element at index " + "" + i + " " +"is"+ " "+ fruits[i] + "<br>")
+// }
